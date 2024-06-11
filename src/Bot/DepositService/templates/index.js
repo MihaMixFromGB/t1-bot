0e79ab7924706f20d515507ad8855e02df917204
an.dehterev <an.dehterev>
2024-06-11 14:53:59 +0700

    add templates

--- src/Bot/DepositService/templates/index.js
+++ src/Bot/DepositService/templates/index.js
@@ -5,4 +5,9 @@ export const DISAGREE_REQUEST = ["нет", "не согласен"];
 export const ACCEPT_RESPONSE = "Отлично! Вклад открыт!";
 export const DISAGREE_RESPONSE =
   "Жаль, что условия вам не подошли. Приходите еще";
+export const UNDEFINED_DEPOSIT_RESPONSE =
+  "Указанного вами вклада нет. Выберите другой";
 export const UNDEFINED_RESPONSE = "Я не понял ваш запрос";
+
+export const ERROR_REQUEST_RESPONSE =
+  "При запросе вкладов произошла ошибка. Попробуйте выполнить запрос позднее";
